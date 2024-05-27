8b4133f889dcd88522a84afb0199a51989384ed9
Sunil S. Singh <sunil.surendra.singh7@gmail.com>
2024-05-27 17:37:00 +0800

    removed clearcache

--- django_project/urls.py
+++ django_project/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/clearcache/", include("clearcache.urls")),
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
     path("accounts/", include("accounts.urls")),
